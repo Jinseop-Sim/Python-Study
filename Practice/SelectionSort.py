0fe674d15b286775de8f81011f2f47b38766f226
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2021-06-29 16:22:24 +0900

    Update SelectionSort.py

--- Practice/SelectionSort.py
+++ Practice/SelectionSort.py
@@ -4,12 +4,13 @@ for i in range(0, len(select)):
     for j in range(i+1, len(select)):
         if(select[j] < select[min]):
             min = j
-            
-    temp = select[i]
-    select[i] = select[min]
-    select[min] = temp
+
+    select[i], select[min] = select[min], select[i]
 
 print(select)
 
 ```
 선택 정렬 알고리즘 연습 코드 입니다.
+파이썬은 타 언어와는 다르게 Swap 기능을 제공합니다.
+list[0], list[1] = list[1], list[0] 의 형태로 제공합니다.
+```
