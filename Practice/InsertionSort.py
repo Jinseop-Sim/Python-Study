27c4d3c0fa98aa27d1cd343ab9240dd9c2eaa85c
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2021-07-01 11:56:45 +0900

    Update InsertionSort.py

--- Practice/InsertionSort.py
+++ Practice/InsertionSort.py
@@ -2,13 +2,14 @@ ins = list(map(int, input().split()))
 
 for i in range(1, len(ins)):
     key = ins[i]
-    for j in range(0, len(ins)):
-        if(key < ins[j]):
-            ins[i], ins[j] = ins[j], ins[i]
+    j = i-1
+    while j>=0 and ins[j] > key:
+        ins[j+1] = ins[j]
+        j -= 1
+    ins[j+1] = key
 
 print(ins)
 
 ```
 O(N^2)의 시간복잡도를 가지는, 삽입 정렬 알고리즘이다.
-굳이 key 변수를 따로 만들 필요는 없지만, 가독성을 위해 만들었다.
 ```
