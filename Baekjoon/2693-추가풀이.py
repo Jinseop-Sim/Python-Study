39469f9bfa1be10c2d1cd903e41b0283f66dcaee
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2021-07-03 12:06:47 +0900

    Update 2693-추가풀이.py

--- Baekjoon/2693-추가풀이.py
+++ Baekjoon/2693-추가풀이.py
@@ -2,9 +2,17 @@ case = int(input())
 
 for i in range(0, case):
     val = list(map(int, input().split()))
-    val.sort()
+
+    for j in range(0, len(val)):
+        minindex = j
+        for k in range(j+1, len(val)):
+            if(val[k] < val[minindex]):
+                minindex = k
+
+        val[j], val[minindex] = val[minindex], val[j]
     print(val[len(val)-3])
     
 ```
 내림차순 정렬을 한 다음, 뒤에서 세 번째 값이 세 번째로 큰 값이 됨을 간단하게 알 수 있다.
+선택정렬을 사용하였다.
 ```
