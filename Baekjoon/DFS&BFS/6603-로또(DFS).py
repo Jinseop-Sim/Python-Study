139c75568f6c34a068fc26e70f4811ecb402cd65
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2021-07-21 20:59:31 +0900

    Update 6603-로또(DFS).py

--- Baekjoon/DFS&BFS/6603-로또(DFS).py
+++ Baekjoon/DFS&BFS/6603-로또(DFS).py
@@ -1,25 +1,25 @@
-def dfs(depth):
+def backtrack(depth):
     if depth == 6:
-        print(*arr)
         return
     for i in range(n):
-        print(arr)
-        if check[i] != 0:
+        if check[i] != 0: # 중복된 수를 넣지 않기 위한 체크 배열.
             continue
-        arr.append(nums[i])
+        visit.append(num[i])
         check[i] = 1
-        dfs(depth+1)
-        arr.pop()
-        for j in range(i+1, n):
-            check[j] = 0
+        print(visit)
+        backtrack(depth+1) # 각 자리는 depth를 나타낸다.
+        visit.pop()
+        for k in range(i+1, n): # 끝날 때 마다 check를 비워주어야 중복이 안됐는데 출력을 못하는 경우가 없다.
+            check[k] = 0
+
 while True:
     lotto = list(map(int, input().split()))
-    n, nums = lotto[0], lotto[1:]
+    n, num = lotto[0], lotto[1:]
     if n == 0:
         break
-    arr = []
-    check = [0]*n
-    dfs(0)
+    visit = []
+    check = [0] * n
+    backtrack(0)
     print()
 
 """
