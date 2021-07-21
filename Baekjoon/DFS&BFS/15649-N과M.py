29dee9207e48d875ed02f0067413a06f514a5982
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2021-07-21 20:10:11 +0900

    Update 15649-N과M.py

--- Baekjoon/DFS&BFS/15649-N과M.py
+++ Baekjoon/DFS&BFS/15649-N과M.py
@@ -6,11 +6,11 @@ def backtrack():
         print(*visit)
         return
     for i in range(1, num+1):
-        if i in visit:
+        if i in visit: # 이 조건은 visit에 같은 i가 2번 들어오는 경우, 즉 중복을 없애기 위한 조건.
             continue
         visit.append(i)
-        backtrack()
-        visit.pop()
+        backtrack() # visit의 길이가 pick과 같으면 바로 출력시켜 버리므로, 깊이는 2를 넘지 않는다.
+        visit.pop() # 하나씩 제외시켜서 그 다음 i를 집어넣는 식.
 
 backtrack()
 
