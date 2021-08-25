78f92ca399e7be3603a12c52451d3ff27a283957
Jinseop-Sim <71700079+Jinseop-Sim@users.noreply.github.com>
2021-08-25 21:55:43 +0900

    Update 1062-가르침.py

--- Baekjoon/DFS&BFS/1062-가르침.py
+++ Baekjoon/DFS&BFS/1062-가르침.py
@@ -3,24 +3,24 @@ import sys
 def dfs(idx, depth):
     global result
 
-    if depth == length-5:
+    if depth == length-5: # 깊이가 최대가 되면 return을 해야하는 조건이다.
         cnt = 0
-        for word in words:
-            check = 1
+        for word in words: # 
+            check = True
             for w in word:
-                if not learn[ord(w) - ord('a')]:
-                    check = 0
+                if not learn[ord(w) - ord('a')]: # 배운 알파벳으로 이루어져있지 않아서 알 수 없는 단어라면, check = False로 바꾸고 다음 단어로.
+                    check = False
                     break
-            if check:
+            if check: # Check가 False가 아니라는 것은, 배울 수 있는 단어라는 뜻! 따라서 cnt를 1 증가 시킨다.
                 cnt += 1
         result = max(result, cnt)
         return
 
     for i in range(idx, 26):
-        if not learn[i]:
+        if not learn[i]: # a부터 z까지 아직 배우지 않은 알파벳을 하나씩 대입해본다.
             learn[i] = 1
             dfs(i, depth + 1)
-            learn[i] = 0
+            learn[i] = 0 # length - 5까지 문자열을 다 채우고, 하나 빼고 그 다음 문자열을 찾아보는 백트래킹.
 
 num, length = map(int, sys.stdin.readline().split())
 if length<5:
@@ -37,11 +37,11 @@ for _ in range(num):
 learn = [0]*26
 
 for c in ['a', 'c', 'i', 'n', 't']:
-    learn[ord(c) - ord('a')] = 1
+    learn[ord(c) - ord('a')] = 1   # a, c, i, n, t는 무조건 배우는 알파벳이다.
 
 dfs(0, 0)
 print(result)
 
 """
-백트래킹 문제였다.
+백트래킹 문제였다... 어렵다.
 """
